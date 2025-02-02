b4a21be21157330d36c2fd561c2dd3e5587960a9
prateekjain13 <jainprateekjp13@gmail.com>
2025-02-02 11:17:22 +0530

    [Prateek] Add: Python script to check and calculate attendance and daily wages for part-time employee

--- emp.py
+++ emp.py
@@ -1,19 +1,20 @@
 import random
 
 def check_attendance():
-    return random.randint(0, 1)
+    return random.randint(0, 1)  
 
 def emp_daily_wage():
     wage_per_hour = 20
     emp_check = check_attendance()
     
     if emp_check == 1:
-        daily_wage = wage_per_hour * 8
-        print(f"The employee is present for the full day, so the daily wage is: {daily_wage}")
+        daily_wage = wage_per_hour * 4
+        print(f"The employee is present, so the daily wage is: {daily_wage}")
     else:
         daily_wage = 0
-        print(f"The employee is absent for the day, so the daily wage is: {daily_wage}")
+        print(f"The employee is absent, so the daily wage is: {daily_wage}")
 
 
 emp_daily_wage()
+
 
