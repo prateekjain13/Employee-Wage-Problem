ab41c5212e739340b73adc17aabeb9c2d5b58356
prateekjain13 <jainprateekjp13@gmail.com>
2025-02-02 11:23:35 +0530

    [Prateek] Add: Python script to check and calulate attendance and wages using switch case statements

--- emp.py
+++ emp.py
@@ -1,20 +1,21 @@
 import random
 
 def check_attendance():
-    return random.randint(0, 1)  
+    return random.randint(0, 1)
 
 def emp_daily_wage():
     wage_per_hour = 20
     emp_check = check_attendance()
     
-    if emp_check == 1:
-        daily_wage = wage_per_hour * 4
-        print(f"The employee is present, so the daily wage is: {daily_wage}")
-    else:
-        daily_wage = 0
-        print(f"The employee is absent, so the daily wage is: {daily_wage}")
-
+    match emp_check:
+        case 1:
+            daily_wage = wage_per_hour * 8
+            print(f"The employee is present, so the daily wage is: {daily_wage}")
+        case _:
+            daily_wage = 0
+            print(f"The employee is absent, so the daily wage is: {daily_wage}")
 
 emp_daily_wage()
+
 
 
