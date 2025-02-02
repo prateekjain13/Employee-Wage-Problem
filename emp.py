ca9a4028e26ab32356e47d6a15f6a6383aab0cd4
prateekjain13 <jainprateekjp13@gmail.com>
2025-02-02 11:41:28 +0530

    [Prateek] Add: Python script to calculate wage till the condition is met

--- emp.py
+++ emp.py
@@ -5,17 +5,21 @@ def check_attendance():
 
 def emp_monthly_wage():
     wage_per_hour = 20
-    working_days_in_month = 20
+    total_working_hours = 100
+    total_working_days = 20
+    total_hours_worked = 0
+    total_days_worked = 0
     total_wage = 0
     
-    for i in range(working_days_in_month):
+    while total_hours_worked < total_working_hours and total_days_worked < total_working_days:
         emp_check = check_attendance()
         if emp_check == 1:
+            total_hours_worked += 8
+            total_days_worked += 1
             total_wage += wage_per_hour * 8
-        else:
-            total_wage += 0
     
     print(f"The total monthly wage for the employee is: {total_wage}")
 
 emp_monthly_wage()
+
 
